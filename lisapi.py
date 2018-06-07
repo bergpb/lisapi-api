8020273f02b07da8dafced2a488a16d9c5fea63f
Berg Paulo <lindembergdepaulo@gmail.com>
2018-06-07 16:36:19 -0300

    Update lisapi.py
    fix text in api return

--- lisapi.py
+++ lisapi.py
@@ -18,20 +18,20 @@ def getStatus():
 
   status = {}
 
-  temp = commands.getoutput("vcgencmd measure_temp | cut -c 6-12")
+  temp = commands.getoutput("vcgencmd measure_temp | cut -c 6-9")
 
   quantProc = commands.getoutput("ps -aux | wc -l")
 
-  mem_total = commands.getoutput("free -h | grep 'Mem' | cut -c 16-19")
-  mem_used = commands.getoutput("free -h | grep 'Mem' | cut -c 29-31")
-  mem_free = commands.getoutput("free -h | grep 'Mem' | cut -c 41-43")
+  mem_total = commands.getoutput("free -h | grep 'Mem' | cut -c 16-18")
+  mem_used = commands.getoutput("free -h | grep 'Mem' | cut -c 28-30")
+  mem_free = commands.getoutput("free -h | grep 'Mem' | cut -c 41-42")
 
   uptime = commands.getoutput("uptime -p")
 
-  total = commands.getoutput("df -h | grep '/dev'| cut -c 18-20 | head -1")
-  used = commands.getoutput("df -h | grep '/dev'| cut -c 23-26 | head -1")
-  free = commands.getoutput("df -h | grep '/dev'| cut -c 30-32 | head -1")
-  percent = commands.getoutput("df -h | grep '/dev'| cut -c 35-37 | head -1")
+  total = commands.getoutput("df -h | grep '/dev'| cut -c 18-29 | head -1")
+  used = commands.getoutput("df -h | grep '/dev'| cut -c 23-25 | head -1")
+  free = commands.getoutput("df -h | grep '/dev'| cut -c 30-31 | head -1")
+  percent = commands.getoutput("df -h | grep '/dev'| cut -c 35-36 | head -1")
 
   date = commands.getoutput("date")
 
@@ -42,32 +42,31 @@ def getStatus():
   tx_wifi = commands.getoutput("cat /sys/class/net/wlan0/statistics/tx_bytes")
   tx_float = float(tx_wifi)
   tx_float_mb = tx_float / 1024 / 1024
-  
+
   ip_lan = commands.getoutput("ifconfig wlan0 |  grep inet | cut -c 14-26 | head -1")
 
   status = ({
-                  'temperature': temp,
-                  'proccess' : quantProc,
-                  'memory' : {
-                      'total': mem_total,
-                      'used' : mem_used,
-                      'free' : mem_free
-                  },
-                  'sdcard' :  {
-                      'total' : total,
-                      'used' : used,
-                      'free' : free,
-                      'percent' : percent
-                  },
-                    'network' :  {
-                      'ip_lan' : ip_lan,
-                      'network_in' : rx_float_mb,
-                      'network_out' : tx_float_mb
-                  },
-              })
-  print(status)
+              'temperature': temp,
+              'proccess' : quantProc,
+              'memory' : {
+                  'total': mem_total,
+                  'used' : mem_used,
+                  'free' : mem_free
+              },
+              'sdcard' :  {
+                  'total' : total,
+                  'used' : used,
+                  'free' : free,
+                  'percent' : percent
+              },
+                'network' :  {
+                  'ip_lan' : ip_lan,
+                  'network_in' : rx_float_mb,
+                  'network_out' : tx_float_mb
+              },
+          })
+
   return jsonify(status = status)
-  print('--------------------------------')
 
 @app.route('/api/pins', methods=['GET'])
 def getStatePins():
@@ -80,12 +79,11 @@ def getStatePins():
     gpio.setup(pin, gpio.OUT)
     state = gpio.input(pin)
     state_pins = {
-                  'pin': pin, 
+                  'pin': pin,
                   'state': state
                   }
-    print (state_pins)
+
     pins.append(state_pins)
-    print('--------------------------------')
 
   return jsonify({'status_pins': pins})
 
@@ -123,15 +121,10 @@ def setStatePins(pin):
     pass
 
   state_pins = {'pin': pin, 'state': state}
-  print (state_pins)
+
   return jsonify({'state_pins': state_pins})
-  print('--------------------------------')
+
 
 
 if __name__=='__main__':
-  #port = port = int(os.environ.get('PORT', 5050))
-  #app.run(debug=True, host='0.0.0.0', port=port)
-  app.run()
-
-
-
+    app.run()
