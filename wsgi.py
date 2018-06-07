26e59d3230599789ebfec0e509526a4d001d4050
Berg Paulo <lindembergdepaulo@gmail.com>
2018-06-07 20:58:32 -0300

    Update wsgi.py
    port changed

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ import os
 from lisapi import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
